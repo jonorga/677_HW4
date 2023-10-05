8012d56c9e6ed6fb9cd2f937340db7f346b919ca
jonorgan <jonorgan26@gmail.com>
2023-10-05 15:32:02 -0400

    Started second PDF

---
+++ Week Labeling/hw4_exam.py
@@ -0,0 +1,57 @@
+###
+### CS667 Data Science with Python, Homework 4, Jon Organ
+###
+
+import pandas as pd
+import math
+Nonetype = type(None)
+
+file_cmg = pd.read_csv("cmg_with_color.csv")
+file_spy = pd.read_csv("spy_with_color.csv")
+
+
+
+def GenerateDF(working_file):
+	output = pd.DataFrame(columns=['Week', 'Color', 'Avg Return', 'Volatility'])
+	file_len = len(working_file.index)
+	i = 0
+	count = 0
+	while i < file_len:
+		if working_file["Color"].get(i) == "Undefined":
+			i = file_len
+		elif working_file["Weekday"].get(i) == "Friday":
+			week_len = 1
+			while working_file["Weekday"].get(i + week_len) != "Friday":
+				week_len += 1
+
+			j = 0
+			avg_return = 0
+			while j < week_len:
+				avg_return += working_file["Return"].get(i + j)
+				j += 1
+			avg_return /= week_len
+
+			volatility = 0
+			dist_from_avg = 0
+			j = 0
+			while j < week_len:
+				dist_from_avg += abs(working_file["Return"].get(i + j) - avg_return) ** 2
+				j += 1
+			volatility = math.sqrt(dist_from_avg / week_len)
+
+			if working_file["Color"].get(i + week_len) != "Undefined":
+				output.at[count, "Week"] = count + 1
+				output.at[count, "Color"] = working_file["Color"].get(i + week_len)
+				output.at[count, "Avg Return"] = avg_return
+				output.at[count, "Volatility"] = volatility
+			count += 1
+			i = i + week_len - 1
+		i += 1
+	return output
+
+output_cmg = GenerateDF(file_cmg)
+output_spy = GenerateDF(file_spy)
+print(output_cmg)
+
+#output_cmg.at[0, "week"] = 1
+#output_cmg.at[1, "week"] = 3
